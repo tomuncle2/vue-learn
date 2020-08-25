c3affb742f769c7858d69b40bf8e8bbc806ada0d
caidi <1289359053@qq.com>
2020-08-26 00:21:05 +0800

    tj

---
+++ vue-demo/component/login.js
@@ -0,0 +1,9 @@
+var loginComponent = {
+    // 只能由一个顶级的标签，否则不显示
+    template:'<div>' +
+        '<h2>登录页</h2>'+
+        '用户名：<input type="text"><br/>' +
+        '密码：<input type="password"><br/>' +
+        '<button>登录</button>'+
+        '</div>'
+}
---
+++ vue-demo/component/register.js
@@ -0,0 +1,10 @@
+var registerComponent = {
+    // 只能由一个顶级的标签，否则不显示
+    template:'<div>' +
+        '<h2>注册页</h2>'+
+        '用户名：<input type="text"><br/>' +
+        '密码：<input type="password"><br/>' +
+        '确认密码：<input type="password"><br/>' +
+        '<button>注册</button>'+
+        '</div>'
+}
